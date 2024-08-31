f51300cb6829f085421ca006ede4f18c597b1474
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-31 10:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+function createPromise(pNum) {
+	return new Promise((resolve,reject)=>{
+		const time = Math.random()*2+1;
+		setTimeOut(()=>{
+			resolve(pNum);
+		},time*1000);
+	})
+}
+const promis1 = createPromise(1);
