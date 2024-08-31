7167f29a8e65c1b2339eb40f47339439d05b7dfc
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-31 17:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,27 @@
 function createPromise(pNum) {
 	return new Promise((resolve,reject)=>{
 		const time = Math.random()*2+1;
-		setTimeOut(()=>{
-			resolve(pNum);
+		setTimeout(()=>{
+			resolve({pNum,time});
 		},time*1000);
 	})
 }
-const promis1 = createPromise(1);
+const p1 = createPromise(1);
+const p2 = createPromise(2);
+const p3 = createPromise(3);
+
+const startTime = Date.now();
+Promise.all([p1,p2,p3]).then((results)=>{
+	const totalTime = (Date.now()-startTime)/1000;
+	const output = document.getElementById("output");
+	results.forEach((result)=>{
+		const tRow = document.createElement("tr");
+		tRow.innerHTML = `<td>Promise ${result.pNum}</td><td>${result.time}</td> `
+		output.appendChild(tRow);
+	})
+	
+})
+
+
+
+
